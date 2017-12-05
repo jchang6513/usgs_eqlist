56ed8fe6993090101f5ce98877972d821cd85df5
jchang6513 <31400914+jchang6513@users.noreply.github.com>
2017-12-05 16:04:30 +0800

    update

--- USGS_example.m
+++ USGS_example.m
@@ -1,7 +1,7 @@
 clear
 clc
 
-% example 1: usgs_eqlist(syear,smonth,sday,eyear,emonth,eday)
+%% example 1: usgs_eqlist(syear,smonth,sday,eyear,emonth,eday)
 
 syear = 2000;
 smonth = 1;
@@ -13,7 +13,7 @@ eday = 1;
 
 [data1, describe1] = usgs_eqlist(syear,smonth,sday,eyear,emonth,eday);
 
-% example 2: usgs_eqlist(syear,smonth,sday,eyear,emonth,eday,'Optional',Value)
+%% example 2: usgs_eqlist(syear,smonth,sday,eyear,emonth,eday,'Optional',Value)
 
 eyear = 2005;
 emonth = 1;
@@ -27,11 +27,11 @@ Mlat = 25;
 
 [data2, describe2] = usgs_eqlist(syear,smonth,sday,eyear,emonth,eday,'minmag',mmag,'minlong',mlong,'maxlong',Mlong,'minlat',mlat,'maxlat',Mlat);
 
-% example 3: usgs_eqlist(syear,smonth,sday,eyear,emonth,eday,'Optional',Value,'Others','Value')
+%% example 3: usgs_eqlist(syear,smonth,sday,eyear,emonth,eday,'Optional',Value,'Others','Value')
 
 [data3, describe3] = usgs_eqlist(syear,smonth,sday,eyear,emonth,eday,'minmag',mmag,'minlong',mlong,'maxlong',Mlong,'minlat',mlat,'maxlat',Mlat,'limit','30','orderby','time-asc');
 
-% example 4: 
+%% example 4: 
 
 eyear = 2005;
 emonth = 1;
